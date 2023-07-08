b9182c955546c145f748bc769348618c196a5026
Anurag Malhotra <anurag.malhotra@dotpe.in>
2023-07-09 00:27:18 +0530

    review files added

--- src/main/java/dev/malhotraji/movies/MovieRepository.java
+++ src/main/java/dev/malhotraji/movies/MovieRepository.java
@@ -2,13 +2,9 @@ package dev.malhotraji.movies;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
-@EnableMongoRepositories
 public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
     Optional<Movie> findMovieByImdbId(String imdbId);
 }
---
+++ src/main/java/dev/malhotraji/movies/ReviewRepository.java
@@ -0,0 +1,7 @@
+package dev.malhotraji.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
